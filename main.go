fe2027dac486e5f89661f5a2b540c239c1f0ddd2
Danil <danildenisov572@gmail.com>
2026-01-10 18:53:22 +0500

    stroke

--- main.go
+++ main.go
@@ -3,7 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var lol string = "lol"
-	fmt.Println(lol)
-	fmt.Println("hehe")
+	var text = "lol"
+	var text1 = "kek"
+	var score int8 = 0
+	fmt.Println(text, text1)
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
+	fmt.Println("Вы пролетели через трубу")
+	score = score + 1
+	fmt.Println("Ваш счет:", score)
 }
