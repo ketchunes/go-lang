cc45825b000175811f22ffba8176398025c860dd
Danil <danildenisov572@gmail.com>
2026-01-11 14:04:08 +0500

    if-1

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var lol int8 = 15
-	if lol >= 10 && lol < 15 {
-		fmt.Println("ты красавчик")
-	} else if lol >= 15 {
-		fmt.Println("Ты мега красавчик")
+	var lol int8 = 50
+	if lol == 12 {
+		fmt.Println("Дюжина")
+	} else if lol == 21 {
+		fmt.Println("Очко")
+	} else if lol == 50 {
+		fmt.Println("Полтос")
 	} else {
-		fmt.Println("Попробуй еще")
+		fmt.Println("ты не попал в категорию")
 	}
+	// if lol >= 10 && lol < 15 {
+	// 	fmt.Println("ты красавчик")
+	// } else if lol >= 15 {
+	// 	fmt.Println("Ты мега красавчик")
+	// } else {
+	// 	fmt.Println("Попробуй еще")
+	// }
 }
