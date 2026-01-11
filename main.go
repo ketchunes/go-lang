8f123ab829842b061fa6f6f9ae71576172a37926
Danil <danildenisov572@gmail.com>
2026-01-11 19:46:58 +0500

    for

--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var lol int8 = 50
-	if lol == 12 {
-		fmt.Println("Дюжина")
-	} else if lol == 21 {
-		fmt.Println("Очко")
-	} else if lol == 50 {
-		fmt.Println("Полтос")
-	} else {
-		fmt.Println("ты не попал в категорию")
+	for i := 0; i <= 6; i++ {
+		fmt.Println(i)
 	}
+	// var lol int8 = 50
+	// if lol == 12 {
+	// 	fmt.Println("Дюжина")
+	// } else if lol == 21 {
+	// 	fmt.Println("Очко")
+	// } else if lol == 50 {
+	// 	fmt.Println("Полтос")
+	// } else {
+	// 	fmt.Println("ты не попал в категорию")
+	// }
 	// if lol >= 10 && lol < 15 {
 	// 	fmt.Println("ты красавчик")
 	// } else if lol >= 15 {
