fabe058ac92980cd5d2efc32f68ece6060480f4e
Danil <danildenisov572@gmail.com>
2026-01-13 23:48:55 +0500

    function

--- main.go
+++ main.go
@@ -3,24 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i <= 6; i++ {
-		fmt.Println(i)
-	}
-	// var lol int8 = 50
-	// if lol == 12 {
-	// 	fmt.Println("Дюжина")
-	// } else if lol == 21 {
-	// 	fmt.Println("Очко")
-	// } else if lol == 50 {
-	// 	fmt.Println("Полтос")
-	// } else {
-	// 	fmt.Println("ты не попал в категорию")
-	// }
-	// if lol >= 10 && lol < 15 {
-	// 	fmt.Println("ты красавчик")
-	// } else if lol >= 15 {
-	// 	fmt.Println("Ты мега красавчик")
-	// } else {
-	// 	fmt.Println("Попробуй еще")
-	// }
+	fmt.Println("Вызываю square:")
+	square(4)
+	square(12)
+	square(10)
+	fmt.Println("Закончил выполнение")
+	zakaz("danil", "spagetti")
+}
+func square(x int) {
+	fmt.Println("Мы присвоили в функцию переменную x:", x)
+	fmt.Println("x в квадрате:", x*x)
+}
+func zakaz(name, lol string) {
+	fmt.Println("Накрываем стол")
+	fmt.Println("Привет:", name)
+	fmt.Println("Ваш заказ:", lol)
+	fmt.Println("Принести:", lol)
 }
