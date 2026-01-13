122605a335935184f413a80c9945703d03ac2034
Danil <danildenisov572@gmail.com>
2026-01-14 00:41:56 +0500

    defer

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Вызываю square:")
-	square(4)
-	square(12)
-	square(10)
-	fmt.Println("Закончил выполнение")
-	zakaz("danil", "spagetti")
+	fmt.Println("Функция main")
+	defer func() {
+		fmt.Println("Анонимная функция main")
+	}()
+	hello()
 }
-func square(x int) {
-	fmt.Println("Мы присвоили в функцию переменную x:", x)
-	fmt.Println("x в квадрате:", x*x)
-}
-func zakaz(name, lol string) {
-	fmt.Println("Накрываем стол")
-	fmt.Println("Привет:", name)
-	fmt.Println("Ваш заказ:", lol)
-	fmt.Println("Принести:", lol)
+func hello() {
+	fmt.Println("Функция hello")
+	defer func() {
+		fmt.Println("defer 1")
+	}()
+	defer func() {
+		fmt.Println("defer 2")
+	}()
+	defer func() {
+		fmt.Println("defer 3")
+	}()
+	fmt.Println("lol")
 }
