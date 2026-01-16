0f994c1bc4695ba8ac6656f17cdcc56926b43439
Danil <danildenisov572@gmail.com>
2026-01-16 19:59:49 +0500

    ukaz1

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	number := 10
 	pointer := &number
-	foo(pointer)
+	kek(pointer)
+	fmt.Println(pointer)
 }
 func foo(n *int) {
 	fmt.Println(n)
+	fmt.Println(*n)
+}
+func kek(a int) {
+	a = 5
 }
