ccadaf78614512454678a37096c7b916ce3a708b
Danil <danildenisov572@gmail.com>
2026-01-19 17:55:35 +0500

    struct1

--- main.go
+++ main.go
@@ -1,25 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type User struct {
-	Name    string
-	Age     int
-	Number  string
-	IsClose bool
-	Raiting float64
-}
-type User1 struct {
-	Name    string
-	Raiting float64
+// type User struct {
+// 	Name    string
+// 	Age     int
+// 	Number  string
+// 	IsClose bool
+// 	Raiting float64
+// }
+// type User1 struct {
+// 	Name    string
+// 	Raiting float64
+// }
+
+// func (u User1) Greeting() {
+// 	fmt.Println("Всем привет")
+// 	fmt.Println("Меня зовут:", u.Name)
+// 	fmt.Println("Мой рейтинг:", u.Raiting)
+// }
+// func (u User1) Goodbye() {
+
+// 	fmt.Println("Меня зовут", u.Name)
+// 	fmt.Println("Всем пока")
+// 	fmt.Println("Мой рейтинг щас", u.Raiting)
+// }
+
+type Product struct {
+	ImgURL      string
+	Name        string
+	Description string
+	Price       int
 }
 
-func Greeting(u User1) {
-	fmt.Println("Всем привет")
-
-	fmt.Println("Меня зовут:", u.Name)
-}
 func main() {
+	// user := User1{
+	// 	Name:    "Максим",
+	// 	Raiting: 6.0,
+	// }
+	// user.Greeting()
+	// user.Goodbye()
+
+	smartphone := Product{
+		ImgURL:      "https://imgsite/image.png",
+		Name:        "Samsung s92000",
+		Description: "Bomba telephone",
+		Price:       100000000,
+	}
+
+	fmt.Println("Image of phone: ", smartphone.ImgURL)
+	fmt.Println("Name of phone: ", smartphone.Name)
+	fmt.Println("Description of phone: ", smartphone.Description)
+	fmt.Println("Price of phone: ", smartphone.Price)
+
 	// user := User{
 	// 	Name:    "Катя",
 	// 	Age:     21,
