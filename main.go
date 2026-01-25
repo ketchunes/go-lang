1dff9c4b154873b61093a8fb2c2b556ef8e6ce19
Danil <danildenisov572@gmail.com>
2026-01-25 10:50:21 +0500

    slice1

--- main.go
+++ main.go
@@ -9,61 +9,7 @@ type User struct {
 }
 
 func main() {
-	userArray := []User{
-		User{
-			Name:    "Данил",
-			Raiting: 5.5,
-			Premium: true,
-		},
-		User{
-			Name:    "Рома",
-			Raiting: 3.1,
-			Premium: false,
-		},
-		User{
-			Name:    "Федя",
-			Raiting: 1.9,
-			Premium: true,
-		},
-		User{
-			Name:    "Катя",
-			Raiting: 9.1,
-			Premium: true,
-		},
-	}
-	fmt.Println("len", len(userArray))
-	fmt.Println("cap", cap(userArray))
-	userArray = append(userArray,
-		User{
-			Name:    "Ваня",
-			Raiting: 2.2,
-			Premium: true,
-		},
-	)
-	fmt.Println("len", len(userArray))
-	fmt.Println("cap", cap(userArray))
-	// fmt.Println("До:")
-	for index, user := range userArray {
-		fmt.Println(index, user)
-	}
-	// // for i := 0; i < len(userArray); i++ {
-	// // 	fmt.Println(userArray[i])
-	// // }
-	// fmt.Println("")
-
-	// for index, user := range userArray {
-	// 	if user.Premium {
-	// 		userArray[index].Raiting += 1.0
-	// 	}
-	// }
-	// // for i := 0; i < len(userArray); i++ {
-	// // 	if userArray[i].Premium {
-	// // 		userArray[i].Raiting += 1.0
-	// // 	}
-	// // }
-	// fmt.Println("После:")
-	// for _, user := range userArray {
-	// 	fmt.Println(user)
-	// }
-
+	intSlice := make([]int, 0)
+	intSlice = append(intSlice, 10, 15, 20, 150)
+	fmt.Println(intSlice)
 }
