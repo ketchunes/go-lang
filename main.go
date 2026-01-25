4f19695ea58634f1efc4dbb852c9ea8a3f83159f
Danil <danildenisov572@gmail.com>
2026-01-25 11:51:29 +0500

    map

--- main.go
+++ main.go
@@ -9,7 +9,16 @@ type User struct {
 }
 
 func main() {
-	intSlice := make([]int, 0)
-	intSlice = append(intSlice, 10, 15, 20, 150)
-	fmt.Println(intSlice)
+	weather := map[int]int{
+		11: +3,
+		12: +6,
+		13: +9,
+		14: -4,
+		15: +1,
+	}
+	for k, v := range weather {
+		fmt.Println(k, v)
+	}
+	weather[20] = 32
+	fmt.Println(weather[20])
 }
