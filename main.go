24f5ca5c68c9135310fbad2d0fd908d0926b8a72
Danil <danildenisov572@gmail.com>
2026-03-20 10:33:11 +0500

    in

--- main.go
+++ main.go
@@ -8,24 +8,37 @@ type Auto interface {
 
 type BMW struct{}
 
-func (b BMW) StepOnGas() {
-	fmt.Println("вы нажали на газ bmv")
-}
-
 type Zhiga struct{}
 
 func (z Zhiga) StepOnGas() {
-	fmt.Println("я жига из танков лол")
+	fmt.Println("Жига")
+}
+
+func (b BMW) StepOnGas() {
+	fmt.Println("Я бмв")
+}
+
+func rideBmw(bmw BMW) {
+	fmt.Println("Я водитель")
+	fmt.Println("Я сажусь в машину")
+	fmt.Println("Я нажмаю на газ")
+	bmw.StepOnGas()
+}
+
+func rideZhiga(zhiga Zhiga) {
+	fmt.Println("Я водитель")
+	fmt.Println("Я сажусь в машину")
+	fmt.Println("Я нажмаю на газ")
+	zhiga.StepOnGas()
 }
 
 func ride(auto Auto) {
-	fmt.Println("я водитель")
-	fmt.Println("я сажусь в машину")
-	fmt.Println("и нажимаю на газ")
+	fmt.Println("Я водитель")
+	fmt.Println("Я сажусь в машину")
+	fmt.Println("Я нажмаю на газ")
 	auto.StepOnGas()
 }
 func main() {
-	// bmw := BMW{}
-	zhiga := Zhiga{}
-	ride(zhiga)
+	bmw := BMW{}
+	ride(bmw)
 }
